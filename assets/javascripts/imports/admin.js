5939ede77a77afca45d3bc4e573a7064552e2dd7
Andy Smith <asmith.esq@gmail.com>
2012-12-26 18:16:16 -0600

    select2 to track type

--- assets/javascripts/imports/admin.js
+++ assets/javascripts/imports/admin.js
@@ -5,21 +5,21 @@ $(document).ready(function() {
 
   /* Change labels when a collection is set to 'broadcast'
   -------------------------------------------------- */
-  function updateBroadcastLabels() {
+  updateBroadcastLabels = function() {
     var collectionType = $('#acf-collection_info tbody td:eq(2) option:selected').text();
     var band = $('.acf-th-band');
     var location = $('.acf-th-location');
 
     if( collectionType == 'Broadcast' ) {
-          band.text('Radio Host');
+      band.text('Radio Host');
       location.text('Radio Station');
     }
     else if( collectionType == 'Play' ) {
-          band.text('Troupe');
+      band.text('Troupe');
       location.text('Location');
     }
     else {
-          band.text('Band');
+      band.text('Band');
       location.text('Location');
     }
   }
@@ -32,12 +32,15 @@ $(document).ready(function() {
     updateBroadcastLabels();
   })
 
+
 });
 
 /* Add Select2 to ACF
 -------------------------------------------------- */
 
 $(document).live('acf/setup_fields', function(e, div){
+  // Add Select2 to track type
+  $('#acf-track_type select').select2({width:'100%'});
 
   $('table .row').each(function() {
     $(this).find('select').select2({width:'100%'});
